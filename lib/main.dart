5c89b3f33ed9daa5deda29f467953264add0ec84
unknown <ali123456aaaq00@gmail.com>
2024-07-13 11:04:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:akaad/screen/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ScreenUtilInit(designSize: Size(375, 812), child: AuthScreen()),
+    );
+  }
+}
